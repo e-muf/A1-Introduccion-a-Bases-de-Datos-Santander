75923b8c3e3f9f037265d3e0fe27e3f66c47dd06
Emanuel Flores <mufymes@gmail.com>
2020-11-09 18:51:24 -0600

    Retos Sesion 3

--- Sesion-03/Retos-Solucion/reto03.sql
+++ Sesion-03/Retos-Solucion/reto03.sql
@@ -4,6 +4,9 @@ from empleado e
 join puesto p
 on e.id_puesto = p.id_puesto;
 
+select * FROM puestos19;
+
+create or replace view empleado_articulo19 as
 select v.clave, concat(e.nombre, ' ', e.apellido_paterno) nombre_empleado, a.nombre
 from empleado e
 join venta v
@@ -11,6 +14,9 @@ on e.id_empleado = v.id_empleado
 join articulo a
 on v.id_articulo = a.id_articulo;
 
+select * FROM empleado_articulo19;
+
+create or replace view puesto_ventas19 as
 select p.nombre, count(clave) total
 from puesto p
 join empleado e
@@ -19,4 +25,7 @@ join venta v
 on v.id_empleado = e.id_empleado
 group by p.nombre;
 
-
+select *
+from puesto_ventas
+order by total desc
+limit 1;
